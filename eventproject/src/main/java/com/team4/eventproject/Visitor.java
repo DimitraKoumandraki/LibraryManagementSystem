344743482be9bb00b64f25f5be8ad42993356a86
DimitraKoumandraki <koumandrakidm@gmail.com>
2025-01-09 11:58:45 +0200

    create visitor1

--- eventproject/src/main/java/com/team4/eventproject/Visitor.java
+++ eventproject/src/main/java/com/team4/eventproject/Visitor.java
@@ -1,6 +1,49 @@
 package com.team4.eventproject;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Visitor {
-	//j
+	 private String name;
+     private String surname;
+     private String email;
+   
+
+     public Visitor(String name, String surname, String email) {
+         this.name = name;
+         this.surname = surname;
+         this.email = email;
+      
+     }
+
+     
+     public String getName() {
+         return name;
+     }
+
+     public void setName(String name) {
+         this.name = name;
+     }
+
+     public String getSurname() {
+         return surname;
+     }
+
+     public void setSurname(String surname) {
+         this.surname = surname;
+     }
+
+     public String getEmail() {
+         return email;
+     }
+
+     public void setEmail(String email) {
+         this.email = email;
+     }
+
+
+
+
 
 }
+
+
